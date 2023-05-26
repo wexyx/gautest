8f23b9216e44ad132c522edbbf8de9f57796dd88
xuyangze <xuyangze@bytedance.com>
2023-05-26 11:00:04 +0800

    first commit
    Change-Id: I8b78b6426babeb06920d2470bafd77eb066d76ba

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wexyx/gautest
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,11 @@
+/**
+ * @author leo
+ * @date 2023/5/26 10:59
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
